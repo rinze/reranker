96f23c717808d98d57e00228fa7a41928cbec967
José María Mateos <chema@rinzewind.org>
2015-06-21 17:21:58 -0400

    Config sample

---
+++ config-sample.py
@@ -0,0 +1,6 @@
+# Full path to the DB file created with the schema.sqlite3 file.
+# DBPATH = "/home/me/whatever/reranker/db/dbfile.db"
+DBPATH = ""
+
+# Expiration time for links, in seconds. 86400 = 1 day
+LINKEXPIRE = 86400   
