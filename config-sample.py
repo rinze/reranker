ac94939c1e17d7b7e8a9d804dc678cc367aab5d5
José María Mateos <chema@rinzewind.org>
2015-06-24 17:30:51 -0400

    New variables

--- config-sample.py
+++ config-sample.py
@@ -3,4 +3,16 @@
 DBPATH = ""
 
 # Expiration time for links, in seconds. 86400 = 1 day
-LINKEXPIRE = 86400   
+LINKEXPIRE = 86400  
+
+# Number of seconds for a link not to age
+LINKFRESH = 7200 
+
+# Number of articles in front page
+ARTICLES_FRONT = 20
+
+# Output directory for the static HTML files. Will be created
+# (along with the necessary subdirectories) if it does not
+# exist.
+# OUTPUTDIR = "/home/me/whatever/reranker/output/"
+OUTPUTDIR = ""
